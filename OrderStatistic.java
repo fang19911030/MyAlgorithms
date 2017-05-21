ebdef7cdc8c6b6c00906f8ce7205753daab327d7
fang19911030 <fang291831388@gmail.com>
2017-05-20 20:39:59 -0400

    add 9.3.9

--- OrderStatistic.java
+++ OrderStatistic.java
@@ -116,6 +116,7 @@ public class OrderStatistic {
         System.out.println(solution.randomizedSelect(A,0,A.length-1,4));
 //        int[][] nums={{0,7},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,8}};
 //        System.out.print(solution.getYcoord(nums));
+        System.out.println(2.5%1);
     }
 
 
